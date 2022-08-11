69875fd8ab22044a3b58f8df2daaf2cf5d99ebe4
jkpark <inmosty@gmail.com>
2022-08-11 23:01:31 +0900

    템플릿 메서드 패턴과 콜백 패턴

---
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -0,0 +1,16 @@
+package hello.advanced;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.TreadLocalLogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+    @Bean
+    public LogTrace logTrace(){
+        //return new FiledLogTrace();
+        return new TreadLocalLogTrace();
+
+    }
+}
